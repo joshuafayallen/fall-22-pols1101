a1b0a027d5f55930fb7683d7043e2c0477b9f82f
joshuafayallen <jallen108@gsu.edu>
2022-08-10 14:52:49 -0400

    add slides

---
+++ static/slides/Class-04/constitution_pdf.R
@@ -0,0 +1,7 @@
+xaringan::decktape("https://spols110122.netlify.app/slides/class-05/federalism", output = "Class-05/federalism.pdf")
+
+
+source("https://git.io/xaringan2pdf")
+
+
+xaringan_to_pdf("https://spols110122.netlify.app/slides/class-05/federalism", output_file = "Class-05/federalism.pdf", include_partial_slides = TRUE)
---
+++ static/slides/pdf-maker.R
@@ -0,0 +1,14 @@
+
+source("https://git.io/xaringan2pdf")
+
+
+
+
+### Federalism 
+xaringan_to_pdf("https://spols110122.netlify.app/slides/class-05/federalism", output_file = "Class-05/federalism.pdf", include_partial_slides = TRUE)
+
+### Exam Review
+### 
+
+
+xaringan_to_pdf("https://spols110122.netlify.app/slides/exam-1-review/exam-1", output_file = "Exam-1-review/exam-1.pdf", include_partial_slides = TRUE)
